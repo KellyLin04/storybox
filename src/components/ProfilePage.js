5acd9ac35b436f83f35879c633916b8bf6e636db
Doris Wei <dwdoriswei@gmail.com>
2021-03-25 22:47:54 -0700

    added sprites

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -1,6 +1,7 @@
 import react from 'react'
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import Box from './Box.js'
+import axios from 'axios'
 
 function ProfilePage({googleObj}){
 
@@ -17,6 +18,24 @@ function ProfilePage({googleObj}){
         console.log(searchValue)
     }
 
+    const icon_api = process.env.icon_api_key;
+    const searchTerm = searchValue
+    const icon_url = 'https://search.icons8.com/api/iconsets/v5/search?term=' + searchTerm + '&platform=cotton&token=' + icon_api
+
+    const [icon, setIcon] = useState('')
+
+    useEffect(()=>{
+        axios.get(icon_url)
+        .then(response => {
+                setIcon(response.data.icons[0].commonName)
+        })
+        .catch((error) => {
+            console.log(error)
+        });
+    }, [icon_url])
+
+    let url = JSON.stringify(icon)
+    url = url.slice(1,-1)
 
     return(
         <div>
@@ -32,6 +51,10 @@ function ProfilePage({googleObj}){
                 <textarea name="text" wrap="soft" onChange={getSearchValue}/>
                 <br/>
                 <button> Search for sprite </button>
+                <img 
+                src = {'https://img.icons8.com/cotton/40/000000/' + url + '.png'}
+                alt  = "image"
+                />   
             </div>
         </div>
     )
